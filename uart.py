eccbc5917c41b8f274bf8344d25acdcdd6bd487d
benzdht3 <156509838+benzdht3@users.noreply.github.com>
2024-05-07 18:09:14 +0700

    Add files via upload

--- uart.py
+++ uart.py
@@ -1,4 +1,5 @@
 import serial.tools.list_ports
+import time
 def getPort():
     ports = serial.tools.list_ports.comports()
     N = len(ports)
@@ -10,9 +11,8 @@ def getPort():
             splitPort = strPort.split(" ")
             commPort = (splitPort[0])
     return "COM3"
-
 if getPort() != None:
-    ser = serial.Serial( port=getPort(), baudrate=115200)
+    ser = serial.Serial(port=getPort(), baudrate=115200)
     print(ser)
 
 mess = ""
@@ -31,21 +31,28 @@ def processData(client,data):
         client.publish("button1", splitData[2])
     if splitData[1] == "P":
         client.publish("button2", splitData[2])
+    
 
 mess = ""
 def readSerial(client):
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(client, mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
+    try:
+        bytesToRead = ser.inWaiting()
+        if (bytesToRead > 0):
+            global mess
+            mess = mess + ser.read(bytesToRead).decode("UTF-8")
+            print(mess)
+            while ("#" in mess) and ("!" in mess):
+                start = mess.find("!")
+                end = mess.find("#")
+                processData(client, mess[start:end + 1])
+                time.sleep(0.5)
+                if (end == len(mess)):
+                    mess = ""
+                else:
+                    mess = mess[end+1:]
+    except KeyboardInterrupt:
+        client.publish("Ack","2")
+        time.sleep(2)
 
 def writeData(data):
     ser.write(str(data).encode("utf-8"))
